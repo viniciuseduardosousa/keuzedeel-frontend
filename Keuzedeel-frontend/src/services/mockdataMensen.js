b21c66660fbbda9d78d034fc433c89af08f23ddf
vinicius sousa <vinigui.sousa@hotmail.com>
2025-05-02 21:54:57 +0200

    alle data toegevoegd

--- Keuzedeel-frontend/src/services/mockdataMensen.js
+++ Keuzedeel-frontend/src/services/mockdataMensen.js
@@ -1,28 +1,43 @@
 const mockMensenData = [
     {
-        name: "John Doe",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Anneke de Zwart",
+        link: "https://www.amsterdam.nl/pmb/mensen/anneke-zwart/",
+        image: "src/assets/images/Mensen/Anneke-de-Zwart.webp"
     },
     {
-        name: "Jane Smith",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Caroline Huszti",
+        link: "https://www.amsterdam.nl/pmb/mensen/caroline-huszti/",
+        image: "src/assets/images/Mensen/Caroline-Huszti.webp"
     },
     {
-        name: "Alice Johnson",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Caroline van Rhijn",
+        link: "https://www.amsterdam.nl/pmb/mensen/caroline-van-rhijn/",
+        image: "src/assets/images/Mensen/Caroline-van-Rhijn.webp"
     },
     {
-        name: "Bob Brown",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "David Brandwagt",
+        link: "https://www.amsterdam.nl/pmb/mensen/david-brandwagt/",
+        image: "src/assets/images/Mensen/David-Brandwagt.webp"
     },
     {
-        name: "Bob Brown",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Hester Ombre",
+        link: "https://www.amsterdam.nl/pmb/mensen/hester-ombre/",
+        image: "src/assets/images/Mensen/Hester-Ombre.webp"
+    },
+    {
+        name: "Jurjen Mellema",
+        link: "https://www.amsterdam.nl/pmb/mensen/jurjen-mellema/",
+        image: "src/assets/images/Mensen/Jurjen-Mellema.webp"
+    },
+    {
+        name: "Marianne Volkers",
+        link: "https://www.amsterdam.nl/pmb/mensen/marianne-volkers/",
+        image: "src/assets/images/Mensen/Marianne-Volkers.webp"
+    },
+    {
+        name: "Sjaak Huijsman",
+        link: "https://www.amsterdam.nl/pmb/mensen/sjaak-huijsman/",
+        image: "src/assets/images/Mensen/Sjaak-Huijsman.webp"
     }
 ];
 
--- Keuzedeel-frontend/src/services/mockdataOpdrachten.js
+++ Keuzedeel-frontend/src/services/mockdataOpdrachten.js
@@ -1,28 +1,88 @@
 const mockOpdrachtData = [
     {
-        name: "John Doe",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Aanpak jeugdwerkloosheid",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/aanpak/",
+        image: "src/assets/images/Projecten/Aanpak-jeugdwerkloosheid.webp"
     },
     {
-        name: "Jane Smith",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Accommodaties EK Atletiek 2016",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/accommodaties-ek/",
+        image: "src/assets/images/Projecten/accomodaties_ek_atletiek.webp"
     },
     {
-        name: "Alice Johnson",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Amstelkwartier",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/amstelkwartier/",
+        image: "src/assets/images/Projecten/amstelkwartier.webp"
     },
     {
-        name: "Bob Brown",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Amsterdamse Impuls Schoolpleinen",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/amsterdamse-impuls/",
+        image: "src/assets/images/Projecten/Amsterdamse-Impuls-Schoolpleinen.webp"
     },
     {
-        name: "Bob Brown",
-        link: "https://www.google.com",
-        image: "https://via.placeholder.com/150"
+        name: "Bureau Broedplaatsen",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/broedplaatsen/",
+        image: "src/assets/images/Projecten/broedplaatsen.webp"
+    },
+    {
+        name: "Coördinatieteam asielzoekers / Lola Lik",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/coordinatieteam/",
+        image: "src/assets/images/Projecten/Coördinatieteam-asielzoekers-Lola-Lik.webp"
+    },
+    {
+        name: "Accommodaties EK Atletiek 2016",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/accommodaties-ek/",
+        image: "src/assets/images/Projecten/Europees-Voorzitterschap-2016.webp"
+    },
+    {
+        name: "Gezonde Schoolgebouwen Amsterdam",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/gezonde-scholen/",
+        image: "src/assets/images/Projecten/Gezonde-Schoolgebouwen-Amsterdam.webp"
+    },
+    {
+        name: "Herinrichting Leidseplein",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/herinrichting/",
+        image: "src/assets/images/Projecten/Herinrichting-Leidseplein.webp"
+    },
+    {
+        name: "Lelylaan",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/lelylaan/",
+        image: "src/assets/images/Projecten/lelylaan.webp"
+    },
+    {
+        name: "Nazorg na incidenten",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/nazorg-incidenten/",
+        image: "src/assets/images/Projecten/nazorg_na_incidenten.webp"
+    },
+    {
+        name: "Sluisbuurt",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/sluisbuurt/",
+        image: "src/assets/images/Projecten/sluisbuurt.webp"
+    },
+    {
+        name: "Sprong over het IJ",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/sprong-ij/",
+        image: "src/assets/images/Projecten/Sprong-over-het-IJ.webp"
+    },
+    {
+        name: "Vernieuwing Erfpachtstelsel",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/vernieuwing/",
+        image: "src/assets/images/Projecten/vernieuwing_erfpachtstelsel.webp"
+    },
+    {
+        name: "Westpoort en sloterdijk",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/westpoort/",
+        image: "src/assets/images/Projecten/westpoort_en_sloterdijken.webp"
+    },
+    {
+        name: "Zelfbouw",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/zelfbouw/",
+        image: "src/assets/images/Projecten/zelfbouw.webp"
+    },
+    {
+        name: "Zuidas/ZuidasDok",
+        link: "https://www.amsterdam.nl/pmb/opdrachten-projecten/zuidas-zuidasdok/",
+        image: "src/assets/images/Projecten/zuidas.webp"
     }
 ];
 

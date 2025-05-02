4b5e497065c2995a6bd62d4c7a4e5cf5b06de96d
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-02 02:15:29 +0200

    Expertise page bulk af, wachten op data refinement

--- Keuzedeel-frontend/src/pages/Expertise.jsx
+++ Keuzedeel-frontend/src/pages/Expertise.jsx
@@ -1,11 +1,31 @@
-
+import ExpertiseSlider from "../components/ExpertiseSlider";
+import ExpertiseAccordion from "../components/ExpertiseAccordion";
 
 const Expertise = () => {
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-6">Verhaal Detail</h1>
-      {/* Voeg hier je verhaal detail content toe */}
+    <div className="container mx-auto px-4 py-24 ">
+      {/* Header sectie met foto en titel */}
+      <div className="flex flex-col md:flex-row items-center gap-12 mb-8 animate-fadeIn">
+        {/* Tekst sectie */}
+        <div className="text-center md:text-left">
+          <h1 className="text-4xl md:text-4xl font-bold mt-6 text-gray-800">Expertise</h1>
+        </div>
+      </div>
+      
+      {/* MensenCard Grid */}
+      <section className="grid grid-cols-1 flex items-center md:grid-cols-2 gap-16">
+        <ExpertiseSlider />
+        <div className="text-md  md:text-xl animate-slideDown">
+          <p>
+          Met meer dan 500 medewerkers verzorgen wij het projectmanagement van complexe multidisciplinaire en integrale projecten in het sociale, fysieke en economische domein. Dat doen wij in opdracht van en samen met andere afdelingen, stadsdelen, wijken en buurten van de gemeente Amsterdam, andere gemeenten en de regio.
+          </p>
+          <p className="mt-4">
+          Ook ontwikkelen we samen met opdrachtgevers en maatschappelijke partners nieuwe kennis en werkwijzen die passen bij de vraagstukken van vandaag en morgen.
+          </p>
+        </div>
+      </section>
+      <section className="py-8"><ExpertiseAccordion/></section>
     </div>
   );
 };
--- Keuzedeel-frontend/tailwind.config.js
+++ Keuzedeel-frontend/tailwind.config.js
@@ -31,3 +31,15 @@ module.exports = {
   },
   plugins: [],
 }
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});

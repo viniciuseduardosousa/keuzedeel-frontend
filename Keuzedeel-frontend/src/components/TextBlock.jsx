3ebca6d2032a0f8878fa5a0bb5c6ba0b960f40bf
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-12 15:21:25 +0200

    SEO changes pt8 + docs update

--- Keuzedeel-frontend/src/components/TextBlock.jsx
+++ Keuzedeel-frontend/src/components/TextBlock.jsx
@@ -3,7 +3,7 @@ const TextBlock = ({ title, description, email}) => {
     return (
       <section className="flex flex-col bg-white md:px-10 py-4">
         <div className="px-8 md:px-16 text-black ">
-            <h3 className="text-lg md:text-2xl font-bold mb-2">{title}</h3>
+            <p className="text-lg md:text-2xl font-bold mb-2">{title}</p>
             <p className="text-sm md:text-base">{description}</p>
             <p className="mb-4">
               <Link aria-label={`email to ${email}`} to={`mailto:${email}`} className="text-blue-500 hover:underline text-xs md:text-base">

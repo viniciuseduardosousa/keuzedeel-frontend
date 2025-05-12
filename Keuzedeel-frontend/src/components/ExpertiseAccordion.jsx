5827e995932bb96947c4952a5c937754b7bf1fbb
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-12 11:04:14 +0200

    SEO changes pt3

--- Keuzedeel-frontend/src/components/ExpertiseAccordion.jsx
+++ Keuzedeel-frontend/src/components/ExpertiseAccordion.jsx
@@ -4,6 +4,7 @@ import {
   AccordionHeader,
   AccordionBody,
 } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const ExpertiseAccordion = () => {
   const [open, setOpen] = React.useState(1);
@@ -16,14 +17,14 @@ const ExpertiseAccordion = () => {
         <AccordionHeader
           onClick={() => handleOpen(1)}
           className={`border-b-0 transition-colors ${
-            open === 1 ? "text-red-500 hover:!text-red-700" : ""
+            open === 1 ? "text-blue-500 hover:!text-blue-700" : ""
           }`}
         >
           Team Wibaut
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         Multidisciplinaire projecten, programma’s en vraagstukken in het ruimtelijk en sociale domein en bedrijfsvoering vragen om ervaring en kennis op het hoogste niveau. Vanuit het Projectmanagementbureau biedt team Wibaut topadviseurs die op interim-basis de functie van manager, regisseur en project- of programmadirecteur vervullen.
-        <a className="text-blue-800 mt-3 hover:underline" href="https://www.amsterdam.nl/pmb/expertise/team-wibaut/"> <br/>Meer over Team Wibaut</a>
+        <Link className="text-blue-800 mt-3 underline" to="https://www.amsterdam.nl/pmb/expertise/team-wibaut/"> <br/>Meer over Team Wibaut</Link>
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 2} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
@@ -37,7 +38,7 @@ const ExpertiseAccordion = () => {
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         Welk resultaat wilt u bereiken? Onze projectmanagers maken heldere en haalbare afspraken en sturen op het resultaat dat u wilt bereiken. Zij houden de beheersaspecten op orde (financiën, organisatie, planning, informatie en communicatie) en betrekken de juiste belanghebbenden en belangstellenden bij het project.
-        <a className="text-blue-800 mt-3 hover:underline" href="https://www.amsterdam.nl/pmb/expertise/projectmanagement/"> <br/>Meer over Projectmanagement</a>
+        <Link className="text-blue-800 mt-3 underline" to="https://www.amsterdam.nl/pmb/expertise/projectmanagement/"> <br/>Meer over Projectmanagement</Link>
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 3} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
@@ -79,7 +80,7 @@ const ExpertiseAccordion = () => {
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         (Her)huisvesting, kantoren, scholen en sportcomplexen: onze bouwmanagers kennen de specifieke Amsterdamse situatie en regels en begeleiden het gehele proces van uw (maatschappelijk) vastgoed. Van voorbereiding, ontwerpmanagement, vergunningentraject, tot aanbesteding en uitvoering van (ver)bouwprojecten.
-        <a className="text-blue-800 mt-3 hover:underline" href="https://www.amsterdam.nl/pmb/expertise/bouwmanagement/"> <br/>Meer over Bouwmanagement</a>
+        <Link className="text-blue-800 mt-3 underline" to="https://www.amsterdam.nl/pmb/expertise/bouwmanagement/"> <br/>Meer over Bouwmanagement</Link>
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 6} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
@@ -93,7 +94,7 @@ const ExpertiseAccordion = () => {
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         Onze maatschappelijk vastgoed adviseurs ontwikkelen in opdracht voorzieningenprogramma's voor transformatie- en stedelijke vernieuwingsgebieden, programma's van eisen voor maatschappelijke voorzieningen en beheer- en exploitatieplannen.
-        <a className="text-blue-800 mt-3 hover:underline" href="https://www.amsterdam.nl/pmb/expertise/bmv/"> <br/>Meer over Maatschappelijk Vastgoed</a>
+        <Link className="text-blue-800 mt-3 underline" to="https://www.amsterdam.nl/pmb/expertise/bmv/"> <br/>Meer over Maatschappelijk Vastgoed</Link>
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 7} className="mb-2 rounded-lg border border-blue-gray-100 px-4">
@@ -107,7 +108,7 @@ const ExpertiseAccordion = () => {
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         In welke fase uw project zich ook bevindt, de planningsadviseurs van het Projectmanagementbureau bieden u altijd een helder en gestructureerd overzicht van uw project. Van een ‘quick-scan’ tot gedetailleerde variantenstudies en scenario’s met diverse oplossingsrichtingen.
-        <a className="text-blue-800 mt-3 hover:underline" href="https://www.amsterdam.nl/pmb/expertise/planning/"> <br/>Planning en Structurering</a>
+        <Link className="text-blue-800 mt-3 underline" to="https://www.amsterdam.nl/pmb/expertise/planning/"> <br/>Planning en Structurering</Link>
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 8} className="rounded-lg border border-blue-gray-100 px-4">
@@ -121,7 +122,7 @@ const ExpertiseAccordion = () => {
         </AccordionHeader>
         <AccordionBody className="pt-0 text-base font-normal">
         Voor meer informatie over het inhuren van onze projectmanagers, bouwmanagers, planningsadviseurs of topadviseurs van de Wibautgroep kunt u contact opnemen met onze afdeling
-        <a className="text-blue-800 mt-3 hover:underline" href="mailto:mg_pmb.mat­chin­ge­nac­count­ma­na­ge­ment@am­ster­dam.nl"> <br/>Matching & Accountmanagement </a>
+        <Link className="text-blue-800 mt-3 underline" to="mailto:mg_pmb.mat­chin­ge­nac­count­ma­na­ge­ment@am­ster­dam.nl"> <br/>Matching & Accountmanagement </Link>
         </AccordionBody>
       </Accordion>
     </>
